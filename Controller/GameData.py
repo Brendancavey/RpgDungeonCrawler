e199d9dc721b10c6036a1a49e86819139025d974
Brendan <bthoeung@gmail.com>
2023-03-23 16:58:13 -0700

    <refactor> Change logic of GameData locations for extensibility.
    Remove hardcoded values of location data.
    Change overworld icon location logic in GameData for extensibility and maintainability.

--- Controller/GameData.py
+++ Controller/GameData.py
@@ -2,6 +2,9 @@ from Model.Entities.Player import Player
 from Model.Entities.Enemy.EnemyList import enemy_list
 from Controller.ObjectsList import potion_list, weapons_list, armor_list, accessories_list
 from Model.Entities.NPC import NPC
+import Model.Entities.Enemy.Enemy
+import Model.Inventory.Item
+import Model.Entities.NPC
 
 
 #testing
@@ -24,10 +27,10 @@ level = 0
 def determineLevel():
     if level == 0:
         location_00 = {'node_pos' : (110,400), 'content' : chad, 'unlock' : [-1, 0]}
-        location_0 = {'node_pos' : (110,250), 'content' : None, 'unlock' : [-1, 0, 1, 2]}
-        location_1 = {'node_pos' : (110,100), 'content' : enemy_list[0], 'unlock' : [0, 1, 3]}
-        location_2 = {'node_pos' : (300,400), 'content' : armor_list[0], 'unlock' : [0, 2, 3, 4]}
-        location_3 = {'node_pos' : (300,200), 'content' : weapons_list[0], 'unlock' : [1, 2, 3]}
+        location_0 = {'node_pos' : (110,250), 'content' : None, 'unlock' : [-1, 0, 2]}
+        location_1 = {'node_pos' : (110,100), 'content' : weapons_list[0], 'unlock' : [0, 1, 3]}
+        location_2 = {'node_pos' : (300,400), 'content' : None, 'unlock' : [0, 2, 3, 4]}
+        location_3 = {'node_pos' : (300,200), 'content' : enemy_list[0], 'unlock' : [1, 2, 3]}
         location_4 = {'node_pos' : (490,400), 'content' : enemy_list[0], 'unlock' : [2, 4, 5, 6]}
         location_5 = {'node_pos' : (490,200), 'content' : accessories_list[0], 'unlock' : [4, 5]}
         location_6 = {'node_pos' : (640,400), 'content' : enemy_list[1], 'unlock' : [4, 6, 7]}
@@ -62,27 +65,39 @@ def determineLevel():
         9: location_9,
         10: location_10
     }
-def determineEnemyLocations():
-    if level == 0:
-        return [1, 4, 6]
-    elif level == 1:
-        return [1, 4, 6]
-def determineTreasureLocations():
-    if level == 0:
-        return [2, 3, 5, 7, 8]
-    elif level == 1:
-        return [2, 3, 5, 7, 8]
-def determineNpcLocations():
-    if level == 0:
-        return [-1]
-    elif level == 0:
-        return [-1]
-
 locations = determineLevel()
+
+def determineEnemyLocations():
+    enemy_locations = []
+    for location in locations.values():
+        if isinstance(location['content'], Model.Entities.Enemy.Enemy.Enemy):
+            for l in locations:
+                if locations[l] == location:
+                    enemy_locations.append(l)
+    print(enemy_locations)
+    return enemy_locations
+def determineTreasureLocations():
+    treasure_locations = []
+    for location in locations.values():
+        if isinstance(location['content'], Model.Inventory.Item.Item):
+            for l in locations:
+                if locations[l] == location:
+                    treasure_locations.append(l)
+    print(treasure_locations)
+    return treasure_locations
+def determineNpcLocations():
+    npc_locations = []
+    for location in locations.values():
+        if isinstance(location['content'], Model.Entities.NPC.NPC):
+            for l in locations:
+                if locations[l] == location:
+                    npc_locations.append(l)
+    print(npc_locations)
+    return npc_locations
+
 enemy_locations = determineEnemyLocations()
 treasure_locations = determineTreasureLocations()
 npc_locations = determineNpcLocations()
-
 
 #inventory grid
 slot_0 = {'slot_pos' : (995,220), 'content' : None}
